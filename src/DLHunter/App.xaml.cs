5e986038e075ac956fbd413ae4d38f8d125bdf9e
nachmore <git@nachmore.com>
2014-09-17 13:51:22 -0700

    Unhandled exception handler

--- src/DLHunter/App.xaml.cs
+++ src/DLHunter/App.xaml.cs
@@ -13,5 +13,14 @@ namespace DLHunter
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Oops, something bad happened. There may be useful information below, if not, email me.\n\n" + e.Exception.ToString());
+        }
     }
 }
